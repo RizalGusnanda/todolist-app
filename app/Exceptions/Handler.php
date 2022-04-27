377ead1473431b0ecfa5b488538462513023496d
= <=>
2022-04-28 00:49:21 +0700

    Pertemuan 11-Praktikum 2

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,12 +1,24 @@
 <?php
-
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
 
 class Handler extends ExceptionHandler
 {
+    public function render($request, $exception)
+    {
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson()) {
+            return response()->json(
+                ['message' => 'Not Found!'],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        return parent::render($request, $exception);
+    }
     /**
      * A list of the exception types that are not reported.
      *
@@ -15,7 +27,6 @@ class Handler extends ExceptionHandler
     protected $dontReport = [
         //
     ];
-
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
@@ -26,7 +37,6 @@ class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
-
     /**
      * Register the exception handling callbacks for the application.
      *
